d122bef364a2c9388fba285b826d2ef49ddf1c1c
astonevart <astonevart@gmail.com>
2017-03-03 21:47:00 +0200

    +navfixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 $(function () {
+    var $menu = $('#menu');
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 150 && $menu.hasClass('normal') ){
+            $menu.removeClass('normal').addClass('fix');
+        } else if($(this).scrollTop() <= 150 && $menu.hasClass("fix")) { $menu.removeClass("fix").addClass("normal"); } });
     $('.carousel').carousel({
         interval: 3000
     });
